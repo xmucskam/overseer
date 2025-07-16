02b9a43c2f2eb2903c2d7a2095264f4a5ff59570
unknown <xmucskam@stuba.sk>
2025-07-16 18:21:35 +0200

    initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+
+
+export default function HomeScreen() {
+  return (
+    <HelloWave />
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
---
+++ app/_layout.tsx
@@ -0,0 +1,25 @@
+import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="dark" />
+    </ThemeProvider>
+  );
+}
