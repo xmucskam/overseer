697298ae4a63e4c425c716e4e06a22303e4b2929
unknown <xmucskam@stuba.sk>
2025-07-18 01:04:23 +0200

    added car records

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,64 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, FlatList, Text, TextInput, View } from 'react-native';
+import { supabase } from '../../utils/supabase';
 
-export default function TabTwoScreen() {
+export default function Home() {
+  const [postText, setPostText] = useState('');
+  const [posts, setPosts] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    const { data, error } = await supabase
+      .from('records')
+      .select('*')
+      .order('created_at', { ascending: false });
+
+    if (error) console.error(error);
+    else setPosts(data);
+  };
+
+  const handleAddPost = async () => {
+    const user = (await supabase.auth.getUser()).data.user;
+    if (!user) return alert('Not logged in');
+
+    const { error } = await supabase.from('posts').insert([
+      {
+        content: postText,
+        user_id: user.id,
+      },
+    ]);
+
+    if (error) console.error(error);
+    else {
+      setPostText('');
+      fetchPosts();
+    }
+  };
+
   return (
-        <View className="flex-1 justify-center items-center bg-white">
-          <Text className="text-2xl font-bold text-blue-500">Hello Tailwind</Text>
-        </View>
+    <View style={{ padding: 16 }}>
+      <TextInput
+        placeholder="What's on your mind?"
+        value={postText}
+        onChangeText={setPostText}
+        style={{ borderWidth: 1, padding: 8, marginBottom: 10 }}
+      />
+      <Button title="Post" onPress={handleAddPost} />
+
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={{ padding: 8, borderBottomWidth: 1 }}>
+            <Text>{item.make} {item.model}</Text>
+            <Text>{item.production_year} </Text>
+            <Text style={{ fontSize: 10, color: '#666' }}>{item.created_at}</Text>
+          </View>
+        )}
+      />
+    </View>
   );
 }
