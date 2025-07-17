c234a6f7a0eb755a29ab24cd23caa0e291bf4d6b
unknown <xmucskam@stuba.sk>
2025-07-17 14:12:51 +0200

    nativewind fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import '../global.css';
 
 export default function RootLayout() {
   const [loaded] = useFonts({
